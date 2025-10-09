b336c94c0aa320fcb26641e837b48c85be702262
Genepro75 <eugene.otuya@student.moringaschool.com>
2025-10-09 19:07:54 +0300

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Alvin Otuya on 09/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhyNotTryTests/WhyNotTryTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryTests.swift
+//  WhyNotTryTests
+//
+//  Created by Alvin Otuya on 09/10/2025.
+//
+
+import Testing
+@testable import WhyNotTry
+
+struct WhyNotTryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
