51ff41e6996ecf5c40cb4497d3837855ce2ac140
Genepro75 <eugene.otuya@student.moringaschool.com>
2025-10-23 13:50:34 +0300

    changes

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -8,17 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+   @State var selected = "Baseball"
+    @State private var id = 1
+    var activities = ["Archery","Bowling","Hiking","Boxing","Cycling","Rugby","Cricket","Baseball","Basketball","Fencing"]
+    
+    var colors: [Color] = [.red, .green, .blue, .orange, .purple, .pink]
+
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Why Not Try")
+                .font(.largeTitle.bold())
+            
         }
-        .padding()
+        VStack {
+            Circle()
+                .fill(colors.randomElement() ?? .blue)
+                .overlay(
+                    Image(systemName: "figure.\(selected.lowercased())")
+                        .font(.system(size: 144))
+                        .foregroundColor(.white)
+                )
+            Text("\(selected)!")
+                .font(.title)
+                .padding()
+                .transition(.slide)
+                .id(id)
+        }
+        Button("Try again") {
+            withAnimation(.easeInOut(duration: 1)) {
+                selected = activities.randomElement() ?? "Baseball"
+                id += 1
+            }
+    }
+        .buttonStyle(.borderedProminent)
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
